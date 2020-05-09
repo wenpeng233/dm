6e772e802ea1ee12f73f82055711996b51ff4602
WenPeng <n014841@archermind.com>
2020-05-09 18:14:04 +0800

    新增统一网关的过滤器

--- dm-gateway-zuul/src/main/java/base/boot/GatewayZuulApp.java
+++ dm-gateway-zuul/src/main/java/base/boot/GatewayZuulApp.java
@@ -1,9 +1,11 @@
 package base.boot;
 
+import base.filter.PreFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -14,5 +16,10 @@ public class GatewayZuulApp {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayZuulApp.class, args);
+    }
+
+    @Bean
+    public PreFilter preFilter(){
+        return new PreFilter();
     }
 }
